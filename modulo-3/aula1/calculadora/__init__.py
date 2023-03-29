68c1a45da5311abab6cbedc3cb67268b19b1c961
Rafael Ribeiro de Lima <ribeirodelimarafael@gmail.com>
2023-03-29 19:49:52 -0300

    feature subtracao

---
+++ modulo-3/aula1/calculadora/__init__.py
@@ -0,0 +1,22 @@
+'''
+from funcoes import soma
+from funcoes import subtracao
+from funcoes import divisao
+from funcoes import multiplicacao
+
+
+def calcule():
+    a = input('Digite um valor: ')
+    b = input('Digite um segundo valor: ')
+
+    while True:
+        funcao = input('Escolha a função desejada (+, -, /, *): ')
+        funcs = {'+': soma, '-': subtracao, '/': divisao, '*': multiplicacao}
+        if funcao in funcs:
+            break
+    
+    print(funcs[funcao](a, b))
+
+calcule()
+'''
+
