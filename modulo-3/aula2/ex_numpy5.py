5188fcb55d378e238a0e57de2841ab484dbbb12d
Rafael Ribeiro de Lima <ribeirodelimarafael@gmail.com>
2023-04-04 16:08:19 -0300

    ex 5

---
+++ modulo-3/aula2/ex_numpy5.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''Crie um array com 40 números aleatórios, distribuídos uniformemente, entre [0, 5).
+
+Para cada elemento do array, calcule:
+
+Seno
+Cosseno
+Log na base 2'''
+
+
+array = np.random.uniform(0, 5, 40)
+
+sen = np.sin(array)
+cos = np.cos(array)
+log2 = np.log2(array)
+
+print(f'Array: \n{array}')
+print(f'\nSeno: \n{sen}')
+print(f'\nCosseno: \n{cos}')
+print(f'\nLog2: \n{log2}')
