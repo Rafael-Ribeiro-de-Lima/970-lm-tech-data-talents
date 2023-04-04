c1ef3ca9f48f682709238a0fff97148b60985a75
Rafael Ribeiro de Lima <ribeirodelimarafael@gmail.com>
2023-04-04 14:51:33 -0300

    ex 3

---
+++ modulo-3/aula2/ex_numpy3.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+'Imprima as dimensões (shape) dos seguintes arrays:'
+
+a = np.array(2)
+
+b = np.array([1, 2, 3, 4, 5])
+
+c = np.array([[1, 2, 3], [4, 5, 6]])
+
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+print(a.shape)
+print(b.shape)
+print(c.shape)
+print(d.shape)
